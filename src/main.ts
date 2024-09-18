6fe17235855a9f6ce6c23cfa0eec2405cfe6358d
Rodrigo Medeiros <rodrigo.rmedeiros20@gmail.com>
2024-09-18 16:26:21 -0300

    ajustes finais adicionando o cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Defina a origem que pode acessar a API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    credentials: true, // Se precisar passar cookies ou credenciais
+  });
+
   await app.listen(3010);
 }
 bootstrap();
